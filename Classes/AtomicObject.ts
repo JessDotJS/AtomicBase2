e667a87b05a5b435dda8e7e4acf824ae2ecc69c8
David Klie <=>
2017-06-15 18:00:01 -0400

    on method done

--- Classes/AtomicObject.ts
+++ Classes/AtomicObject.ts
@@ -5,12 +5,34 @@ export class AtomicObject {
 
     public db: Database;
     public item: any; // I cant figure out the type of this.
-    public eventListenerRef: EventListener;
+    public eventListenerRef: EventListener; // I assume this is the type here.
     public objectRef: firebase.database.Reference;
+    private id: string;
 
-    constructor(db: Database, objectRef: firebase.database.Reference){
+    constructor(db: Database, objectRef: firebase.database.Reference) {
         this.db = db;
         this.item = {};
-        this.objectRef = this.db.ref.root.child();
+        this.objectRef = this.db.ref.root.child() || this.db.ref.primary;
+    }
+
+    public on(): Promise<string> {
+        this.id = "";
+        let executor: (resolve: (value?: string | PromiseLike<string>) => void, reject: (reason: any) => void) => void;
+        executor = (resolve, reject) => {
+            this.db.server
+                .getLatestTS()
+                .then((serverTS: any) => {
+                    this.objectRef.once("value", (snapshot: any) => {
+                        this.item = this.db.schema.build("snapshot", snapshot);
+                        this.id = this.generateInstanceID();
+                        resolve(this.id);
+                    });
+                });
+        };
+        return new Promise<string>(executor);
+    }
+
+    public generateInstanceID(): string {
+        return "";
     }
 }
