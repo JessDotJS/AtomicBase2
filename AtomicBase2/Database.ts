a57ffd903acf6a83ff71ba1dff6144ec9335f190
Lesther Caballero <lesther993@gmail.com>
2017-06-15 16:33:51 -0400

    Database.ts

---
+++ AtomicBase2/Database.ts
@@ -0,0 +1,63 @@
+import {RefRegistrator} from './RefRegistrator' ;
+import {AtomicPriority} from './AtomicPriority';
+import {Schema} from './Schema';
+import {Query} from './Query';
+import {Server} from './Server';
+import {AtomicArray} from './AtomicArray';
+import {AtomicFile} from './AtomicFile';
+
+class Database {
+
+	ref:any;
+	atomicPriority:any;
+	schema:any;
+	query:any;
+	server:any;
+	atomicArray:any;
+	atomicFile:any;
+
+	constructor(databaseObject){
+
+		/*
+	     * Refs Related
+	    	 * */
+	    this.ref = new RefRegistrator(databaseObject.refs);
+
+
+	    this.atomicPriority = new AtomicPriority(databaseObject.schema.priority || null, this.ref);
+
+	    /*
+	     * Schema Related
+	     * */
+	    this.schema = new Schema(databaseObject.schema, this.atomicPriority);
+
+
+	    /*
+	     * Query Related
+	     * */
+	    this.query = new Query(this.ref, this.schema);
+
+
+	    /*
+	     * Server Related
+	     * */
+	    this.server = new Server(this.ref);
+
+
+	    /*
+	     * Atomic Array Related
+	     * */
+	    this.atomicArray = new AtomicArray(this.ref, this.schema, this.server, this.atomicPriority, databaseObject.filters);
+
+
+
+	    /*
+	     * Atomic File Related
+	     * */
+	    this.atomicFile = new AtomicFile(this.ref);
+
+
+	}
+
+
+}
