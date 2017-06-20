4e66ce22e95718fb5397212a5244510983624e99
David Klie <=>
2017-06-20 16:25:32 -0400

    User class created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,20 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: '<your-key>',
-        authDomain: '<your-project-authdomain>',
-        databaseURL: '<your-database-URL>',
-        projectId: '<your-project-id>',
-        storageBucket: '<your-storage-bucket>',
-        messagingSenderId: '<your-messaging-sender-id>'
+        apiKey: "AIzaSyBgD5jpW1RMJ2vWvp-M4h4GEpLWheT7RnM",
+        authDomain: "cryptotraderfetcher.firebaseapp.com",
+        databaseURL: "https://cryptotraderfetcher.firebaseio.com",
+        projectId: "cryptotraderfetcher",
+        storageBucket: "cryptotraderfetcher.appspot.com",
+        messagingSenderId: "740463984281"
+    },
+
+    fetcherAuth: {
+        email: 'fetcher@cryptotrader.com',
+        password: 'CryptoAtomicAbc_'
+    },
+
+    ravenConfig: {
+        url: 'https://16a213d59c024310bf56084c9e208603:a5f78b3fdbe942f18522f01d72b1d506@sentry.io/177356'
     }
 };
