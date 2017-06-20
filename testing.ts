912af84485e4dcddb18303f55ff92526a6e831b5
JessDotJS <jessmanny19@gmail.com>
2017-06-20 14:21:57 -0400

    Testing Array

--- testing.ts
+++ testing.ts
@@ -6,6 +6,36 @@ import {RefRegistrator} from './Classes/RefRegistrator';
 firebase.initializeApp(fetcherConfig.firebaseConfig);
 
 
+
+export const users: any[] = [
+    {
+        name: 'Jesus Graterol',
+        type: 'student',
+        age: '26'
+    },
+    {
+        name: 'David Klie',
+        type: 'teacher',
+        age: '22'
+    },
+    {
+        name: 'Jayme Armstrong',
+        type: 'student',
+        age: '24'
+    },
+    {
+        name: 'Lesther Caballero',
+        type: 'staff',
+        age: '23'
+    },
+];
+
+
+
+
+
+
+
 //let RegistratorClass =  new RefRegistrator();
 //let QueryClass = new Query('');
 
