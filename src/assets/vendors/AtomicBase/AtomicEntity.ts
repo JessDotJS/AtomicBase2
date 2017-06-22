670817907a0fa0d7b5413e13a7f56610ef3ca871
JessDotJS <jessmanny19@gmail.com>
2017-06-22 11:12:35 -0400

    Atomic Entity

--- src/assets/vendors/AtomicBase/AtomicEntity.ts
+++ src/assets/vendors/AtomicBase/AtomicEntity.ts
@@ -1,21 +1,58 @@
 /**
  * Created by jessdotjs on 22/06/17.
  */
-import { Database } from './Database';
+import {RefRegistrator} from './RefRegistrator' ;
+import {AtomicPriority} from './AtomicPriority';
+import {Schema} from './Schema';
+import {Query} from './Query';
+import {AtomicFile} from './AtomicFile';
 
 
 
 
 
 export class AtomicEntity {
-    public db: any;
+    public ref: any;
+    public atomicPriority: any;
+    public schema: any;
+    public query: any;
+    public atomicFile: any;
+
     constructor(dbObject: any) {
+        /*
+         * Refs Related
+         * */
+        this.ref = new RefRegistrator(dbObject.refs);
 
-        this.db = new Database(dbObject);
+        /*
+         * Atomic Priority
+         * */
+        this.atomicPriority = new AtomicPriority(dbObject.schema.priority || null, this.ref);
+
+        /*
+         * Schema Related
+         * */
+        this.schema = new Schema(dbObject.schema, this.atomicPriority);
 
 
+        /*
+         * Query Related
+         * */
+        this.query = new Query(this.ref, this.schema);
+
+
+        /*
+         * Atomic File Related
+         * */
+        this.atomicFile = new AtomicFile(this.ref);
     }
 
+
+    public create(record: any): Promise<any> {
+        return this
+            .query
+            .create(this.schema.build(record, 'primary'));
+    }
 
 }
 
