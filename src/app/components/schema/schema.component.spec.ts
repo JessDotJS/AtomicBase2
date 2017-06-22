3ccb6bc2c2565e9c609077397e4b3320520134cb
JessDotJS <jessmanny19@gmail.com>
2017-06-22 13:20:29 -0400

    Components Order

--- src/app/components/schema/schema.component.spec.ts
+++ src/app/components/schema/schema.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { SchemaComponent } from './schema.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SchemaComponent', () => {
+  let component: SchemaComponent;
+  let fixture: ComponentFixture<SchemaComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ SchemaComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SchemaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
